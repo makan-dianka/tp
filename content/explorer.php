3499931117ddf22c95d9f85d0784752b98dbd513
makan dianka <makan.dianka@hotmail.com>
2022-01-06 21:36:04 +0000

    update explore file

--- content/explorer.php
+++ content/explorer.php
@@ -1,24 +1,64 @@
+<?php
+
+$current = (isset($_GET['folder'])) ? $_GET['folder'] : "/";
+$parent = substr($current,0, strrpos($current, '/'));
+$files = scandir($current);
+$current = $current == "/" ? "" : $current;
+unset($files[0]);
+unset($files[1]);
+?>
+
+
 <div class="container">
     <div class="row" id="test">
-        <div class="col-md-2">
-        </div>
-        <div class="col-md-8">
             <h4>Explorer les fichiers:</h4>
             <hr>
+            <?php if ($current != "") : ?>
 
-            <?= getFolderCard(); ?>
+                <div class='col-3'>
+                    <div class='card'>
+                        <div class='card-body'>
 
+                            <?php if ($parent != "") : ?>
+                                <a href="?page=explorer&folder=<?= $parent?>" style="text-decoration: none; margin: 10px;"> 
+                                    <img src='./img/retour.jpg' width='50' height='50'> Retour 
+                                </a>
+                            <?php else : ?>
+                                <a href="?page=explorer" style="text-decoration: none; margin: 10px;"> 
+                                    <img src='./img/retour.jpg' width='50' height='50'> Retour 
+                                </a>
+                            <?php endif ?>
 
+                        </div>
+                    </div>
+                </div>
 
+            <?php endif ?>
 
-        <!-- <?php foreach ($files as $file) : ?>
+            <?php foreach ($files as $file) : ?>
+                <?php $fullPath = $current . "/" . $file ?>
 
-            <?= getFolderCard(); ?>
+                <div class='col-3'>
+                    <div class='card'>
+                        <div class='card-body'>
 
-        <?php endforeach ?> -->
+                            <?php if (is_dir($fullPath)) : ?>
+                                
+                                <a href="?page=explorer&folder=<?= $fullPath?>" style="text-decoration: none;"> <img src='./img/folder2.png' width='50' height='50'> <?= $file; ?> </a>
 
-        </div>
-        <div class="col-md-2">
-        </div>
+                            <?php else : ?>
+
+                                <img src='./img/file2.png' width='50' height='50'>
+                                <?= $file; ?>
+
+                            <?php endif ?>
+
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach ?>
+
     </div>
+    <br />
 </div>
