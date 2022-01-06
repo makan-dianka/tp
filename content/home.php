c4001870240fa35c670d30b7966eaf58bd183c70
makan dianka <makan.dianka@hotmail.com>
2022-01-06 10:57:40 +0000

    decouvert php

---
+++ content/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <?php include ("./template/bootstrap.php"); ?>
+</head>
+<body>
+    <?php include ("./template/base.php"); ?>
+
+    <div class="container">
+        <div class="row" id="test">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-8" id="bienvenue">
+                <h3>Bienvenue sur la page d'accueille</h3>
+                <hr>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
