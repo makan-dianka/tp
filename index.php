7b4c838f3587e55e7db3326a980dfc78ba856d76
Makan DIANKA <makan.dianka@hotmail.com>
2022-10-11 04:11:26 +0200

    edit: link css img

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Formulaire</title>
         <?php include ("./template/bootstrap.php"); ?>
-        <link rel="stylesheet" href="/assets/css/main.css">
-        <link rel="icon" href="/assets/img/php.jpg">
+        <link rel="stylesheet" href="/php/assets/css/main.css">
+        <link rel="icon" href="/php/assets/img/php.jpg">
     </head>
     <body>
         <!-- include content -->
